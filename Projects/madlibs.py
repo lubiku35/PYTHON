695ef53f3460c300534c95df287fad74cd91210b
ĽUBOŠ MOTOŠICKÝ <107181603+lubiku35@users.noreply.github.com>
2022-07-19 19:13:35 +0200

    Add files via upload

---
+++ Projects/madlibs.py
@@ -0,0 +1,25 @@
+""" Author: lubiku """
+""" Follow @lubiku / @lubiku35 """
+
+# Simple Madlibs Story 
+
+def Madlibs():
+    
+   adjective_1 = input("Adjective: ")
+   verb_3 = input("Verb: ")
+   noun_1 = input("Noun: ")
+   noun_2 = input("Noun: ")
+   adjective_4 = input("Adjective: ")
+   noun_3 = input("Noun: ")
+   adjective_2 = input("Adjective: ")
+   adjective_3 = input("Adjective: ")
+   verb_1 = input("Verb: ")
+   noun_5 = input("Noun: ")
+   verb_2 = input("Verb: ")
+   noun_4 = input("Noun: ")
+
+   madlips = (f"Hello im very {adjective_1} person. What i want to say that i really {verb_3} {noun_1}, but {noun_2} is really {adjective_4}. Is that {noun_3}? Hello {noun_3} you are so {adjective_2} and {adjective_3}. I think i should {verb_1} {noun_5}. Exactly! I will {verb_2}, and see my {noun_4}")
+
+   print(madlips)
+
+Madlibs()
